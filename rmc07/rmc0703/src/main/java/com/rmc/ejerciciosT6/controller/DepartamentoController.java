d2e4af6c5bf8b6b249d5991034b9ec2099bae66b
Rubén Muñoz <rubenmunozcumbreras@gmail.com>
2023-12-05 13:17:56 +0100

    Commit ejercicio 3--Falta mirar pag 94

---
+++ rmc07/rmc0703/src/main/java/com/rmc/ejerciciosT6/controller/DepartamentoController.java
@@ -0,0 +1,75 @@
+package com.rmc.ejerciciosT6.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rmc.ejerciciosT6.domain.Departamento;
+import com.rmc.ejerciciosT6.service.DepartamentoService;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/departamento")
+public class DepartamentoController {
+    
+    @Autowired
+    public DepartamentoService departamentoService;
+
+    
+
+    @GetMapping({ "/", "/list" })
+    public String showListDTO(Model model) {
+        model.addAttribute("listaDepartamentos", departamentoService.obtenerTodos());
+        return "CRUD_Departamento/departamentoView";
+    }
+
+    @GetMapping("/nuevo")
+    public String showNewDTO(Model model) {
+        // el commandobject del formulario es una instancia de empleado vacia
+        model.addAttribute("departamentoForm", new Departamento());
+        return "CRUD_Departamento/FormNewDTO";
+    }
+
+    @PostMapping("/nuevo/submit")
+    public String showNewSubmitDTO(
+            @Valid Departamento departementoForm,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "redirect:/departamento/nuevo";
+        departamentoService.añadir(departementoForm);
+        return "redirect:/departamento/list";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String showEditFormDTO(@PathVariable long id, Model model) {
+        Departamento departamento = departamentoService.obtenerPorId(id);
+        // el commandobject del formulario es el empleado con el id solicitado
+        if (departamento != null) {
+            model.addAttribute("departamentoForm", departamento);
+            return "CRUD_Departamento/FormEditDTO";
+        }
+        // si no lo encuentra vuelve a la página de inicio.
+        return "redirect:/departamento/list";
+    }
+
+    @PostMapping("/editar/submit")
+    public String showEditSubmitDTO(
+            @Valid Departamento departamentoForm,
+            BindingResult bindingResult) {
+        if (!bindingResult.hasErrors())
+            departamentoService.editar(departamentoForm);
+        return "redirect:/departamento/list";
+    }
+
+    @GetMapping("/borrar/{id}")
+    public String showDeleteDTO(@PathVariable long id) {
+        departamentoService.borrar(id);
+        return "redirect:/departamento/list";
+    }
+}
---
+++ rmc07/rmc0703/src/main/java/com/rmc/ejerciciosT6/domain/Empleado.java
@@ -0,0 +1,47 @@
+package com.rmc.ejerciciosT6.domain;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+/*Aqui es donde ponemos las ManyToOne*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+
+public class Empleado {
+   
+    @Id
+    @GeneratedValue
+        private Long id;
+    @NotEmpty
+        private String nombre;
+    @Email(message = "Debe tener formato email valido")
+        private String email;
+    private Double salario;
+    private boolean enActivo;
+    private Genero genero;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Departamento departamento;
+
+    @ToString.Exclude/*@ToString.Exclude a la Categoría ya que como una 
+                        categoría a su vez incluye otros empleados, que 
+                        tienen categoría... se puede producir un bucle infinito.*/
+    @ManyToOne
+    private Categoria categoria;
+}
