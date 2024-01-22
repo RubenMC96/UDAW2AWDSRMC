2719d304fd8c0721eefa4c53bfcad7e4cf97414a
Rubén Muñoz <rubenmunozcumbreras@gmail.com>
2024-01-22 13:16:24 +0100

    Actualizacion 22/01/2024 - 8.11

---
+++ rmc08/rmc0811/src/main/java/com/app/demo/controller/CambioDataController.java
@@ -0,0 +1,61 @@
+package com.app.demo.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import reactor.core.publisher.Mono;
+
+
+
+@Controller
+public class CambioDataController {
+    
+
+    @GetMapping("/")
+    public Mono<ModelAndView> showForm(Model model) {
+        List<String> monedas = Arrays.asList("Euro", "Libra Esterlina", "Yen", "Dolar");
+        model.addAttribute("monedas", monedas);
+        return Mono.just(new ModelAndView("indexView"));
+    }
+
+    @PostMapping("/convertir")
+    @ResponseStatus.ok(HttpStatus.OK)
+    public String convertirMoneda(@RequestParam("importe") Float importe,
+                                    @RequestParam("origen") String origen,
+                                    @RequestParam("destino") String destino,
+                                    Model model) {
+        /*Creamos un mapa en el que indicamos el codigo
+        que le corresponde a cada moneda
+        */
+
+        HashMap<String, String> codMoneda = new HashMap();
+
+        codMoneda.put("Euro","EUR");
+        codMoneda.put("libra esterlina","GBP");
+        codMoneda.put("Libra Esterlina","JPY");
+        codMoneda.put("Dolar","USD");
+        //Con esto podemos asignar el codigo de moneda para hacer la conversion
+
+        String codOrigen = codMoneda.get(origen);
+        String codDestino = codMoneda.get(destino);
+        Float cambio = cambioService.getExchangeRate(codOrigen, codDestino).block();
+        Float resultado = importe * cambio;
+        model.addAttribute("resultado", resultado)
+        
+        return "resultadoView";
+    }
+    
+    
+
+
+}
